4c3f4158856310d35abf5eeef10298dd96504ced
Rowland O'Flaherty <rowoflo@gmail.com>
2018-09-25 11:56:37 -0500

    Added unit tests and fixed date extraction bugs

--- extractor.py
+++ extractor.py
@@ -4,6 +4,8 @@ import os
 import re
 import subprocess
 
+import dateparser
+
 
 def extract_date_from_pdf(input_pdf: str) -> datetime.date:
     """Extract the date from a PDF file
@@ -14,6 +16,8 @@ def extract_date_from_pdf(input_pdf: str) -> datetime.date:
     Returns:
         Date found in file
     """
+    if not os.path.exists(input_pdf):
+        raise FileNotFoundError('Not a valid file: {}'.format(input_pdf))
     process_err = subprocess.call(['pdftotext', input_pdf])
     if process_err:
         raise RuntimeError('Unable to convert PDF to text')
@@ -35,6 +39,9 @@ def extract_date_from_file(input_file: str) -> datetime.date:
 
     Returns:
         Date found in file
+
+    Note:
+        If multiple dates are found, returns the newest date
     """
     with open(input_file) as opened_file:
         return extract_date_from_string(opened_file.read())
@@ -48,9 +55,42 @@ def extract_date_from_string(input_str: str) -> datetime.date:
 
     Returns:
         Date found in string
+
+    Note:
+        If multiple dates are found, returns the oldest date
     """
-    # mm/dd/yyyy format
-    date_pattern = r'(?P<month>\d\d)/(?P<day>\d\d)/(?P<year>\d\d\d\d)'
-    result = re.search(date_pattern, input_str)
-    datedict = {key: int(val) for key, val in  result.groupdict().items()}
-    return datetime.date(**datedict)
+    months = ['january', 'february', 'march', 'april', 'may', 'june',
+              'july', 'august', 'september', 'november' 'december']
+    months_rex_strs = []
+    for month in months:
+        month_rex_str = r''
+        for letter in month[0:3]:
+            month_rex_str += (r'[' + letter.upper() + letter + ']')
+        for letter in month[3:]:
+            month_rex_str += (r'[' + letter.upper() + letter + ']?')
+        months_rex_strs.append(month_rex_str)
+
+    date_patterns = []
+    # Format: m/d/yy to mm/dd/yyyy with / or -
+    date_patterns.append(('MDY', r'((?<!\d)\d\d?)(-|/)((?<!\d)\d\d?)(-|/)(\d\d\d\d|\d\d)'))
+    # Format: yyyy/mm/dd with / or -
+    date_patterns.append(('YMD', r'(\d\d\d\d)(-|/)((?<!\d)\d\d?)(-|/)((?<!\d)\d\d?)'))
+    # MMM dd, yyyy
+    date_patterns.append(('MDY',
+                          r'(' + r'|'.join(months_rex_strs) + r').*?(\d\d?).+?(\d\d\d\d|\d\d)'))
+
+    dates_found = []
+    for order, date_regex_str in date_patterns:
+        these_dates = []
+        for this_date in re.findall(date_regex_str, input_str):
+            if '' not in this_date:
+                these_dates.append((order, this_date))
+        dates_found.extend(these_dates)
+
+
+    oldest_date = None
+    for order, date_found in dates_found:
+        this_date = dateparser.parse('/'.join(date_found), settings={'DATE_ORDER': order})
+        if this_date is not None and (oldest_date is None or this_date < oldest_date):
+            oldest_date = this_date
+    return oldest_date
---
+++ test/extractor_test.py
@@ -0,0 +1,43 @@
+'''Extrator package tests'''
+import datetime
+import os
+
+from .. import extractor
+
+
+def test_extract_date_from_string():
+    '''Test the extract_date_from_string function'''
+    test_cases = [
+        'January 2, 2003',
+        'Jan 2, 2003',
+        'Jan 2 2003',
+        '01/02/2003',
+        '01/02/03',
+        '1/2/03',
+        '2003/01/02',
+        '01-02-2003',
+        '01-02-03',
+        '1-2-03',
+        '2003-01-02',
+    ]
+    expected_date = datetime.datetime(2003, 1, 2)
+    for case in test_cases:
+        actual_date = extractor.extract_date_from_string(case)
+        assert expected_date == actual_date, '{}'.format(case)
+
+
+def test_extract_date_from_pdf():
+    '''Test the extract_date_from_pdf function'''
+    test_data_folder = 'test/data'
+    for root, _, files in os.walk(test_data_folder):
+        for test_file in files:
+            if test_file[0] == '.' or os.path.splitext(test_file)[1] != '.pdf':
+                continue
+            expected_date = datetime.datetime.strptime(
+                test_file[0:10], '%Y-%m-%d')
+            try:
+                actual_date = extractor.extract_date_from_pdf(os.path.join(root, test_file))
+            except Exception as err:
+                print(test_file)
+                raise err
+            assert expected_date == actual_date, '{}'.format(test_file)
